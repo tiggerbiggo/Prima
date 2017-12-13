30458887137ba323616096f111a5d7e13f857ff2
Timothy Leatherbarrow <timothy.leatherbarrow@gmail.com>
2017-12-13 14:28:39 +0000

    Changed Map Generator class
    Modified both the signature and core method of MapGenerator.java. Now
    works on a "top left, bottom right" system.

--- src/main/java/com/tiggerbiggo/prima/presets/MapGenerator.java
+++ src/main/java/com/tiggerbiggo/prima/presets/MapGenerator.java
@@ -1,63 +1,48 @@
 package com.tiggerbiggo.prima.presets;
+
 import com.tiggerbiggo.prima.core.float2;
 import com.tiggerbiggo.prima.processing.fragment.ConstFragment;
 
-public class MapGenerator
-{
-
-    public static float2[][] getMap(int width, int height, float2 offset, float2 scale, MapTypes type) throws IllegalArgumentException
+public class MapGenerator {
+    public static ConstFragment[][] getFragMap(
+            int width,
+            int height,
+            float2 topLeft,
+            float2 bottomRight)
+            throws IllegalArgumentException
     {
         width = Math.max(1, width);
         height = Math.max(1, height);
-        if(offset == null || scale == null)
-            throw new IllegalArgumentException("Either Offset or Scale are null.");
-
-        float2[][] map = new float2[width][height];
-
-        switch(type) {
-            case REGULAR:
-                for (int i = 0; i < width; i++) {
-                    for (int j = 0; j < height; j++) {
-                        float x, y;
-                        x = i / (width / scale.getX());
-                        x += offset.getX();
-
-                        y = j / (height / scale.getY());
-                        y += offset.getY();
-
-                        map[i][j] = new float2(x, y);
-                    }
-                }
-                break;
-        }
-        return map;
-    }
-
-    public static ConstFragment[][] getFragMap(int width, int height, float2 offset, float2 scale, MapTypes type) throws IllegalArgumentException
-    {
-        width = Math.max(1, width);
-        height = Math.max(1, height);
-        if(offset == null || scale == null)
+        if (topLeft == null || bottomRight == null)
             throw new IllegalArgumentException("Either Offset or Scale are null.");
 
         ConstFragment[][] map = new ConstFragment[width][height];
 
-        switch(type) {
-            case REGULAR:
-                for (int i = 0; i < width; i++) {
-                    for (int j = 0; j < height; j++) {
-                        float x, y;
-                        x = i / (width / scale.getX());
-                        x += offset.getX();
+        float x1, x2, y1, y2, dx, dy;
 
-                        y = j / (height / scale.getY());
-                        y += offset.getY();
+        x1=topLeft.getX();
+        x2=bottomRight.getX();
 
-                        map[i][j] = new ConstFragment(new float2(x, y));
-                    }
-                }
-                break;
+        y1=topLeft.getY();
+        y2=bottomRight.getY();
+
+        dx = x2-x1;
+        dy = y2-y1;
+
+        dx/=width;
+        dy/=height;
+
+        for(int i=0; i<width; i++){
+            for(int j=0; i<height; j++){
+                map[i][j] = new ConstFragment(
+                        new float2(
+                                x1+(i*dx),
+                                y1+(i*dy)
+                        )
+                );
+            }
         }
-        return map;
+
+        return null;
     }
 }
