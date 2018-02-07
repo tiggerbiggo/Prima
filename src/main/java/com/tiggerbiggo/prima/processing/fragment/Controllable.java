f81c2cb4d609e5768fc760074b57cd9e0ba72f7e
tiggerbiggo <timothy.leatherbarrow@gmail.com>
2018-02-07 20:39:22 +0000

    deleted duplicate files
    Be careful with floobits!!!

--- src/main/java/com/tiggerbiggo/prima/processing/fragment/Controllable.java
+++
@@ -1,8 +0,0 @@
-package com.tiggerbiggo.prima.processing.fragment;
-
-import com.sun.istack.internal.NotNull;
-import com.tiggerbiggo.prima.gui.ControlPane;
-
-public interface Controllable {
-    ControlPane getControls(ControlPane p);
-}
